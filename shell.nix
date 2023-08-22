90fe3da0742cfaf3888aaa3059e7e392fd537d0e
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-22 13:32:23 -0300

    nixified!

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+    my-python-packages = ps: with ps; [ 
+      (#SpeechRecognition
+        buildPythonPackage rec {
+          pname = "SpeechRecognition";
+          version = "3.10.0";
+          src = fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-FBMRVeiougDq0be5saL6cchF5Ntfml9mozob1sVcbDU=";
+          };
+          doCheck = false;
+          propagatedBuildInputs = [
+            pkgs.python3Packages.requests
+          ];
+        }
+      )
+      #Text to Speech functions inside python
+      pyttsx3
+      
+      #For SpeechRecognition to work properly
+      pyaudio
+      
+      
+    ];
+  in
+
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs.buildPackages; [
+    (pkgs.python3.withPackages my-python-packages)
+    pkgs.arcanPackages.espeak # For ghost to be able to speak
+    pkgs.flac
+  ]; 
+}
